577df0720538eb062da6e31f62deda191522dca5
JuMaLaB <julien.malaboeuf@gmail.com>
2022-01-12 10:18:11 +0100

    init spring boot

---
+++ src/test/java/fr/pluralsight/confdemo/ConfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pluralsight.confdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
